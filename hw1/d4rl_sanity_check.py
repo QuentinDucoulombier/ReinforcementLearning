e4343fde07728de271a721d53d2da5e12283395c
Quentin Ducoulombier <quent1ducou@gmail.com>
2024-03-20 20:37:31 +0800

    chore: update d4rl python script

--- hw1/d4rl_sanity_check.py
+++ hw1/d4rl_sanity_check.py
@@ -10,13 +10,15 @@ import gym
 import d4rl # Import required to register environments, you may need to also import the submodule
 
 # Create the environment
-env = gym.make('maze2d-umaze-v1')
+#env = gym.make('maze2d-umaze-v1')
+# Modify the environment with Hopper dataset
+env = gym.make('hopper-medium-v0') 
 
 # d4rl abides by the OpenAI gym interface
 env.reset()
 env.step(env.action_space.sample())
 
-# Each task is associated with a dataset
+# Each task is associaated with a dataset
 # dataset contains observations, actions, rewards, terminals, and infos
 dataset = env.get_dataset()
 print(dataset['observations']) # An N x dim_observation Numpy array of observations
